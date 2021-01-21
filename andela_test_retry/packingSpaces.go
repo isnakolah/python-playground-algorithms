a940ad55c062c47cae08503bfa0419851e299174
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-01-21 12:41:15 +0300

    Update the parking space

--- andela_test_retry/packingSpaces.go
+++ andela_test_retry/packingSpaces.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 func main() {
 	fmt.Println(packingDilemma([]int{2, 3, 9, 18, 20, 22}, 3))
-	fmt.Println(packingDilemma([]int{2, 5, 6, 7, 9, 10}, 2))
+	fmt.Println(packingDilemma([]int{2, 5, 6, 7, 9, 13, 10}, 2))
 }
 
 func packingDilemma(arr []int, k int) int {
 	min := math.MaxInt64
+	sort.Ints(arr)
 
 	for i := 1; i < len(arr)-k+1; i++ {
 		next := arr[i+k-1]
