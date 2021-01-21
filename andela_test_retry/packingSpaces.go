d8bd3add199a2b0d8187128c9defb3ec09e744ba
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-01-21 11:53:44 +0300

    Finish the packingSpaces.go

---
+++ andela_test_retry/packingSpaces.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(packingDilemma([]int{2, 3, 9, 18, 20, 22}, 3))
+	fmt.Println(packingDilemma([]int{2, 5, 6, 7, 9, 10}, 2))
+}
+
+func packingDilemma(arr []int, k int) int {
+	min := math.MaxInt64
+
+	for i := 1; i < len(arr)-k+1; i++ {
+		next := arr[i+k-1]
+		cur := arr[i]
+
+		temp := next - cur
+
+		if temp < min {
+			min = temp
+		}
+	}
+	return min
+}
